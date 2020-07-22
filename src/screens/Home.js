87028a191a958b08c09ae93f713e93f9fe3d6fd2
Leorna <gabrielcapuerj@gmail.com>
2020-07-21 22:38:39 -0300

    adding modal

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,13 @@
 import React, { useState } from 'react';
-import { View, Text, FlatList, TouchableOpacity } from 'react-native';
+import {
+    View,
+    Text,
+    FlatList,
+    TouchableOpacity,
+    StyleSheet,
+    Modal
+} from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 import Card from '../shared/Card';
 import { globalStyles } from '../styles/global';
 
@@ -10,6 +18,9 @@ export default function Home({ navigation }) {
         { title: 'Game 2', rating: 4, body: 'lorem ipsum', key: '2' },
         { title: 'Game 3', rating: 5, body: 'lorem ipsum', key: '3' }
     ]);
+    const [modal, setModal] = useState(false);
+
+    const toggle = () => setModal(!modal);
 
     const renderItem = ({ item }) => (
         <TouchableOpacity onPress={() => navigation.navigate('ReviewDetails', item)}>
@@ -21,9 +32,47 @@ export default function Home({ navigation }) {
 
     return (
         <View style={globalStyles.container}>
+            <Modal visible={modal} animationType="slide">
+                <View style={styles.modalContent}>
+                    <MaterialIcons
+                        name="close"
+                        size={24}
+                        style={{
+                            ...styles.modalToggle,
+                            ...styles.modalClose
+                        }}
+                        onPress={toggle} />
+                    <Text>Hello from Modal</Text>
+                </View>
+            </Modal>
+
+            <MaterialIcons
+                name="add"
+                size={24}
+                style={styles.modalToggle}
+                onPress={toggle} />
+
             <FlatList
                 data={reviews}
                 renderItem={renderItem} />
         </View>
     );
 }
+
+
+const styles = StyleSheet.create({
+    modalContent: {
+        flex: 1
+    },
+    modalToggle: {
+        marginBottom: 10,
+        borderWidth: 1,
+        borderColor: '#f2f2f2',
+        borderRadius: 10,
+        alignSelf: 'center'
+    },
+    modalClose: {
+        marginTop: 50,
+        marginBottom: 0
+    }
+});
